a67b193ee250ce99170366ee4c2f3626bc5f2356
win10 <85410188@qq.com>
2021-04-02 22:15:59 +0800

    feat method defined

--- src/playground/teypdef.go
+++ src/playground/teypdef.go
@@ -24,7 +24,15 @@ func main() {
 
 	carFuel += ToGallons(Liters(40))
 	busFuel += toLiters(Gallons(30))
-	fmt.Printf("%0.1f:%0.1f", carFuel, busFuel)
+	fmt.Printf("%0.1f:%0.1f\n", carFuel, busFuel)
+
+	soda := Liters(2)
+	fmt.Printf("%0.3f liters equals %0.3f gallons\n", soda, soda.ToGallons())
+	water := Milliliters(500)
+	fmt.Printf("%0.3f liters equals %0.3f gallons\n", water, water.ToGallons())
+	milk := Gallons(2)
+	fmt.Printf("%0.3f gallons equals %0.3f liters\n", milk, milk.ToLiters())
+	fmt.Printf("%0.3f gallons equals %0.3f milliliters\n", milk, milk.ToMilliliters())
 
 }
 
@@ -33,4 +41,17 @@ func ToGallons(l Liters) Gallons {
 }
 func toLiters(g Gallons) Liters {
 	return Liters(g * 3.785)
+}
+
+func (m Liters) ToGallons() Gallons {
+	return Gallons(m * 0.264)
+}
+func (m Milliliters) ToGallons() Gallons {
+	return Gallons(m / 1000 * 0.264)
+}
+func (g Gallons) ToLiters() Liters {
+	return Liters(g * 3.785)
+}
+func (g Gallons) ToMilliliters() Milliliters {
+	return Milliliters(g * 3785.41)
 }
